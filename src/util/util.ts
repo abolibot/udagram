50d88f3546d8daca7c0f2ed1a2f512635cf3a0fb
Oluwatobi <oluwatobi.alaran@mypatricia.co>
2022-08-29 22:08:58 +0100

    implement filteredimage endpoint

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import axios from "axios";
 import Jimp = require("jimp");
 
 // filterImageFromURL
@@ -11,7 +12,11 @@ import Jimp = require("jimp");
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
+      const { data: imageBuffer } = await axios.get(inputURL, {
+        responseType: "arraybuffer",
+      });
+
+      const photo = await Jimp.read(imageBuffer);
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
